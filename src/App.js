caf41835f47a4737cd6b08063fd95ff736259d85
Daniel_Martinez <daniel.umanamartinez@gmail.com>
2023-07-11 17:02:19 -0400

    created routes header and footer skeleton

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<h1>Hello</h1>} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.scss"
+
+
+function Header(){
+    return (
+        <>
+        <h1>Header</h1>
+        </>
+    )
+}
+
+export default Header;
