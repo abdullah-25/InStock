5e0f2f7bf24d20722a841c80c130908f3a2fda74
Daniel_Martinez <daniel.umanamartinez@gmail.com>
2023-07-12 12:36:25 -0400

    added fixes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,19 +3,14 @@ import Logo from "../../assets/logo/InStock-Logo.svg"
 
 
 function Header(){
-
-    function clickHandler(){
-
-    }
-
     return (
         <header className="header">
             <article className="header__logo">
                 <img src={Logo} alt="Instock Logo" className="header__logo-content" />
             </article>
             <article className="header__actions">
-                <button onClick={clickHandler} className="header__actions-button warehouses-button">Warehouses</button>
-                <button onClick={clickHandler} className="header__actions-button inventory-button">Inventory</button>
+                <button className= "header__actions-button warehouses-button" >Warehouses</button>
+                <button  className="header__actions-button inventory-button">Inventory</button>
             </article>
         </header>
         
