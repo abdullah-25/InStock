e2bb38b1f0ca41608f3f2816065fbfa4a49060f9
Daniel_Martinez <daniel.umanamartinez@gmail.com>
2023-07-14 00:59:15 -0400

    added basic ui and inputs

--- src/App.js
+++ src/App.js
@@ -1,77 +1,43 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import EditInventory from "./components/EditInventory/EditInventory";
 
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+// import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-import WarehouseDetails from "./pages/WarehouseDetails/WarehouseDetails.jsx";
+// import WarehouseDetails from "./pages/WarehouseDetails/WarehouseDetails.jsx";
 
-import Warehouses from "./pages/Warehouses/Warehouses";
-import WarehouseList from "./components/WarehouseList/WarehouseList";
+// import Warehouses from "./pages/Warehouses/Warehouses";
+// import WarehouseList from "./components/WarehouseList/WarehouseList";
 
-import { useEffect, useState } from "react";
-import axios from "axios";
+// import { useEffect, useState } from "react";
+// import axios from "axios";
 
-
-import InventoryList from "./components/InventoryList/InventoryList";
-import ItemDetails from "./components/ItemDetails/ItemDetails";
-
+// import InventoryList from "./components/InventoryList/InventoryList";
+// import ItemDetails from "./components/ItemDetails/ItemDetails";
 
 function App() {
-  const [inventoryArray, setInventoryArray] = useState(undefined)
-  useEffect(() => {
-      axios.get("http://localhost:8080/api/inventories").then((response) => {
-    setInventoryArray(response.data[0]);
-  }).catch(response => {
-    console.log(response);
-  })
- }, [])
-  if(!inventoryArray){
-    return (
-      <> Loading...
-      </>
-    )
-  }
-
+  //   const [inventoryArray, setInventoryArray] = useState(undefined)
+  //   useEffect(() => {
+  //       axios.get("http://localhost:8080/api/inventories").then((response) => {
+  //     setInventoryArray(response.data[0]);
+  //   }).catch(response => {
+  //     console.log(response);
+  //   })
+  //  }, [])
+  //   if(!inventoryArray){
+  //     return (
+  //       <> Loading...
+  //       </>
+  //     )
+  //   }
 
   return (
-    <BrowserRouter>
+    <>
       <Header />
-
-      <Routes>
-        <Route path="/details/:id" element={<WarehouseDetails />}/> 
-
-
-      {/* <Link to={`/addwarehouse`} style={{ textDecoration: "none" }}>
-        <div>Add Warehouse</div>
-      </Link> */}
-
-
-        <Route path="/" element={<ItemDetails item={{
-    id: 5,
-    warehouse_name: "Manhatten",
-    item_name: "Shampoo",
-    description: "Natural shampoo made from 99% biodegradable ingredients.",
-    category: "Health",
-    status: "In Stock",
-    quantity: 4350,
-    created_at: "2023-07-12T16:11:34.000Z",
-    updated_at: "2023-07-12T16:11:34.000Z"
-  }}/>} />
-
-         <Route path="/" element={<Warehouses />} />
-
-
-        {/* <Route path="/" element={<InventoryList inventoryArray={inventoryArray} setInventoryArray={setInventoryArray}/>} /> */}
-
-
-        {/* <Route path="/" element={<WarehouseList warehousearray={warehouseArray} setWarehouseArray={setWarehouseArray}/>} /> */}
-
-
-
-      </Routes>
+      <EditInventory />
       <Footer />
-    </BrowserRouter>
+    </>
   );
 }
 
