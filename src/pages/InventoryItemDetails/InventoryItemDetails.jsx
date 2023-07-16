04207b2800c37fa59b807e34c721bec6a6859094
Zachary Ringwood <zringwood@gmail.com>
2023-07-16 13:38:31 -0400

    Adding inventory page

--- src/pages/InventoryItemDetails/InventoryItemDetails.jsx
+++ src/pages/InventoryItemDetails/InventoryItemDetails.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from 'axios';
+import ItemDetails from "../../components/ItemDetails/ItemDetails";
 import "../PagePositioning.scss"
+
 function InventoryItemDetails(){
+    const [item, setItem] = useState(null);
+    const itemID = useParams("id").id;
+    useEffect(() => {
+        axios.get(`/inventories/${itemID}`).then(response => {
+            setItem(response.data)
+        }).catch(response => {
+            console.log(response.data)
+        })
+    }, [])
+    if(!item){
+        return <>
+            Loading...
+        </>
+    }
     return (
         <div className="positioning">
+            <ItemDetails item = {item}/>
         </div>
     )
 }
