62203f4e3fcc31afa6e6d168c3afb11e08e9d992
Zachary Ringwood <zringwood@gmail.com>
2023-07-16 12:59:32 -0400

    Edit link implemented

--- src/components/WarehouseDetailsDisplay/WarehouseDetailsDisplay.jsx
+++ src/components/WarehouseDetailsDisplay/WarehouseDetailsDisplay.jsx
@@ -1,20 +1,21 @@
 
 import "./WarehouseDetailsDisplay.scss";
 import edit from "../../assets/icons/edit-button-24px.svg"; 
-import {Link} from "react-router-dom";
-function WarehouseDetailsDisplay({ warehouse_name, address,city, contact_name, contact_email,contact_phone, contact_position}){
+import {Link} from "react-router-dom"
+function WarehouseDetailsDisplay({ id,warehouse_name, address,city, contact_name, contact_email,contact_phone, contact_position}){
     return (
         <>
         <header className="detailheader">
            
             <button className="detailheader__back" onClick={() => {window.history.back()}}></button>
-          
             <h1 className="detailheader__title">{warehouse_name}</h1>
+            <Link to = {`/edit/${id}`} className="detailheader__edit">
             <button className="detailheader__edit--mobile"></button>
             <button className="detailheader__edit--tablet">
                 <img src = {edit} alt = "" />
-                Edit
+               Edit
                 </button>
+            </Link>
         </header>
         <article className="details">
             <div className="details__title">
